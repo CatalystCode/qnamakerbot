c010fb9e91120466284cc274755e29f6324bf2d8
Toby Bradshaw <tobybrad@microsoft.com>
2016-07-13 13:54:53 +0100

    Succesfully generate : separated training data

--- data/process.js
+++ data/process.js
@@ -1,13 +1,19 @@
 function main() {
 
+  var lines = 0;
+
   var lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('source_faq.txt')
   });
 
   lineReader.on('line', function (line) {
+    if (lines++ < 13) {
+      // First 13 lines are header/meta info
+      return;
+    }
     var parts = line.split("\t");
-    var q = parts[1];//.replace(/:/g, ";");
-    var a = parts[3];//.replace(/:/g, ";");
+    var q = parts[1].replace(/:/g, ";");
+    var a = parts[3].replace(/:/g, ";");
     console.log(q + ":" + a);
   });
 
