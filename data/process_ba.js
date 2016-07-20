694c02fe4e580163288ff865d7037935052fb82f
Toby Bradshaw <tobybrad@microsoft.com>
2016-07-20 14:26:43 +0100

    Add metadata to answers

--- data/process_ba.js
+++ data/process_ba.js
@@ -11,29 +11,35 @@ function main() {
   for (var worksheet of worksheets) {
     for (var row of worksheet.data) {
       if (lines > 0) {
+
         var q = row[2];
         q = striptags(q).trim();
 
         var meta = {};
 
-        meta.action = row[3];
-        meta.cardtype = row[4];
-        meta.image = row[5];
-        meta.introText = row[6];
-        meta.button1 = row[7];
-        meta.button2 = row[8];
-        meta.button1Action = row[9];
-        meta.button2Action = row[10];
+        meta.action = row[3]; // video, identity, location, carousel, richContext
+        meta.cardType = row[4]; // hero, thumbnail
+        meta.mainImage = row[5]; // url
+        meta.introText = row[6]; // markdown
+        meta.button1Text = row[7]; // text
+        meta.button2Text = row[8]; // text
+        meta.button1Action = row[9]; // text
+        meta.button2Action = row[10]; // text, hero
+        meta.carouselImages = row[11]; // carousel images
+        meta.carouselText = row[12]; // carousel text per image
 
-        a = row[3];
+        var a = row[13];
         if (q.indexOf('?') == q.length - 1) {
           if (q in questions) {
             if (questions[q].length > a.length) {
               a = questions[q];
             }
           }
+
           var a = a.replace(/\n/g, "<br/>").trim();
           var a = a.replace(/\r/g, "").trim();
+          a += "<metadata>" + JSON.stringify(meta) + "</metadata>";
+          //console.log(JSON.stringify(meta));
           questions[q] = a;
         }
       }
