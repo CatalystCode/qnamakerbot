78d2bf7b223e43fc88fe1e7f512c70474b23bb6c
tobe <tobybrad@microsoft.com>
2016-09-08 14:17:54 +0100

    Checkpoint

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var uuid = require('node-uuid');
 var request = require('request');
 var querystring = require('querystring');
 
@@ -8,25 +9,30 @@ var botbuilder = require('botbuilder');
 var config = require('nconf').env().argv().file({ file: './localConfig.json' });
 
 function createConsoleConnector() {
-  console.log('Hi.. ask about the Microsoft Bot Framework and I\'ll do my best to answer.');
+  console.log('Hi.. ask me about the Microsoft Bot Framework.');
   return new botbuilder.ConsoleConnector().listen();
 }
 
 function createChatConnector() {
 
   let opts = {
-    appId: config.get('MICROSOFT_APP_ID'),
-    appPassword: config.get('MICROSOFT_APP_PASSWORD')
+    appId: config.get('BOT_APP_ID'),
+    appPassword: config.get('BOT_APP_PASSWORD')
   };
 
   let chatConnector = new botbuilder.ChatConnector(opts);
 
   let server = restify.createServer();
-  server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
-  });
 
   server.post('/qna', chatConnector.listen());
+  server.get(/\/?.*/, restify.serveStatic({
+    directory: __dirname + '/html',
+    default: 'index.html',
+  }));
+
+  server.listen(3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+  });
 
   return chatConnector;
 }
@@ -46,16 +52,32 @@ function qna(q, cb) {
   });
 }
 
-function main() {
+function initialiseBot(bot) {
 
-  let connector = config.get('console') ? createConsoleConnector() : createChatConnector();
-  let bot = new botbuilder.UniversalBot(connector);
   let intents = new botbuilder.IntentDialog();
 
   intents.matches(/^(\/history)/i, [
     function (session) {
       session.send(session.userData.history.join('\n'));
       return
+    }
+  ]);
+
+  intents.matches(/^(\/goto)/i, [
+    function (session) {
+      session.send("Transferring you..");
+      session.send("Click on the link to transfer to the web chat");
+      session.send('http://localhost:3978');
+      session.send("When prompted, use the following token to identify yourself:");
+      let uid = uuid.v4();
+      session.send(uid);
+      return
+    }
+  ]);
+
+  intents.matches(/^(\/firstRun)/i, [
+    function (session) {
+      botbuilder.Prompts.text(session, "Hello... What's your name?");
     }
   ]);
 
@@ -72,12 +94,25 @@ function main() {
         session.send('Unfortunately an error occurred. Try again.');
       }
       else {
+        console.log(result);
         session.send(JSON.parse(result).answer);
       }
     });
   });
 
   bot.dialog('/', intents);
+
+  bot.on('conversationUpdate', function (message) {
+  //  console.log(message);
+  });
+  bot.on('message', function (message) {
+  //  console.log(message);
+  });
+}
+
+function main() {
+  initialiseBot(new botbuilder.UniversalBot(createChatConnector()));
+  initialiseBot(new botbuilder.UniversalBot(createConsoleConnector()));
 }
 
 if (require.main === module) {
